1b7705fe95af735c5fde3866dd043101ecbd84e6
Glenn Halls <gjhalls@gmail.com>
2022-08-10 21:41:28 +1000

    Finished: Debugging component

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
@@ -10,10 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "I hope this shows up!!!")
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello, debugging!"
         logging()
+        division()
     }
 
-    fun division()
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Thread.sleep(3000)
+            Log.d(TAG, "$denominator")
+            findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
 
     fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
